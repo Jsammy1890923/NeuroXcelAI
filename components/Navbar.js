9a4c0f80328b360d0b677184304487f04f8f7ba3
John Phillips <Jsammy1890923@gmail.com>
2025-03-05 00:59:13 -0500

    Added Navbar componen/Update Navbar.js
    - Created responsive Navbar for site navigation  
    - Included links to Assistants, Pricing, and Blog  
    - Styled with Tailwind CSS

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,14 @@
-
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-900 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/" className="text-white text-2xl font-bold">NeuroXcel</a>
+        <ul className="flex space-x-4">
+          <li><a href="/assistants" className="text-gray-300 hover:text-white">Assistants</a></li>
+          <li><a href="/pricing" className="text-gray-300 hover:text-white">Pricing</a></li>
+          <li><a href="/blog" className="text-gray-300 hover:text-white">Blog</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
