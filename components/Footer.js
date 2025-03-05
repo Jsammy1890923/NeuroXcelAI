0614db7b248f94e5e88c46e4cf18be8f3fb54cea
John Phillips <Jsammy1890923@gmail.com>
2025-03-05 03:14:24 -0500

    Added Footer component/Update Footer.js
    - Designed Footer for the website
    - Includes copyright notice
    - Styled with Tailwind CSS

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,7 @@
-
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-300 text-center p-4 mt-10">
+      <p>&copy; 2025 NeuroXcel AI. All rights reserved.</p>
+    </footer>
+  );
+}
